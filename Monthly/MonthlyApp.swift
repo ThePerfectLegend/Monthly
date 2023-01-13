a8a026765807952d4e7e92865864e40f83b1c088
Nizami <nizami@about-me.app>
2023-01-13 13:05:30 +0500

    Initial Commit

---
+++ Monthly/MonthlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MonthlyApp.swift
+//  Monthly
+//
+//  Created by Nizami Tagiyev on 13.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct MonthlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
